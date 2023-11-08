4515daa40c0245b90f77b4deb2f5871787104df6
MegaTophat <98920383+MegaTophat@users.noreply.github.com>
2023-11-08 12:31:16 -0600

    Check to see if .env file already exists and create it if it doesn't

--- src/main/java/anti/sus/Main.java
+++ src/main/java/anti/sus/Main.java
@@ -3,10 +3,34 @@ package anti.sus;
 import net.dv8tion.jda.api.JDA;
 
 import java.io.File;
+import java.io.IOException;
 public final class Main {
     private static final File WORKING_DIRECTORY = new File(System.getProperty("user.dir"));
     private static final File ENV_FILE = new File(WORKING_DIRECTORY, ".env");
     public static void main(String[] args) {
+        createFiles();
         final JDA jda;
+    }
+
+    private static void createFiles() {
+        if (ENV_FILE.isFile()) {
+            return;
+        }
+
+        try {
+            assertState(ENV_FILE.createNewFile(), "isFile() == createNewFile(). Does this user have permission?");
+        } catch (final IOException | IllegalStateException ex) {
+            System.out.println(ex + "\nCreating base files failed: " + ex.getMessage());
+            System.exit(1);
+            return;
+        }
+    }
+
+    public static void assertState(final boolean expression, final String assertionDescription) {
+        if (expression) {
+            return;
+        }
+
+        throw new IllegalStateException("Assertion failed: " + assertionDescription);
     }
 }
