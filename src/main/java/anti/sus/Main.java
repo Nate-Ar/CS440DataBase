13b339dcdcdb428b48990df5557b9b612132f4cd
MegaTophat <98920383+MegaTophat@users.noreply.github.com>
2023-11-08 12:21:13 -0600

    Add constants referencing the token file

--- src/main/java/anti/sus/Main.java
+++ src/main/java/anti/sus/Main.java
@@ -2,7 +2,10 @@ package anti.sus;
 
 import net.dv8tion.jda.api.JDA;
 
+import java.io.File;
 public final class Main {
+    private static final File WORKING_DIRECTORY = new File(System.getProperty("user.dir"));
+    private static final File ENV_FILE = new File(WORKING_DIRECTORY, ".env");
     public static void main(String[] args) {
         final JDA jda;
     }
