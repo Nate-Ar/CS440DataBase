bb6ef3da924d265b15b9a6bbaf30f5959c34a59a
nathanarter <21artern@gmai.com>
2023-12-10 15:50:31 -0600

    ok now its working better fixed the spelling errors

--- report.sql
+++ report.sql
@@ -12,19 +12,20 @@ SELECT * FROM USERS;
 -- filter word ORDER BY number of violations
 SELECT * FROM FILTERED_WORDS ORDER BY numViolations;
 -- messages that are not filtered AND the authorID has 9 in it
-SELECT * FROM MESSAGES WHERE filtered = FALSE AND autherID LIKE "%9%";
+SELECT * FROM MESSAGES WHERE filtered = FALSE AND authorID LIKE "%9%";
 -- admins that have userID with 7 OR 2 in it
 SELECT * FROM ADMINS WHERE userID LIKE "%7%" OR userID LIKE "%2%";
 -- COUNT all users
 SELECT COUNT(userID) FROM USERS;
 -- sum of all violations
+
 SELECT SUM(numViolations) FROM USERS;
 -- avrage numver of violations per user
-SELECT AVG(numViolatons) FROM USERS;
+SELECT AVG(numViolations) FROM USERS;
 -- user with the lowest violations
-SELECT MIN(numViolatons) FROM USERS;
+SELECT MIN(numViolations) FROM USERS;
 -- user with the Highest violations
-SELECT MAX(numViolatons) FROM USERS;
+SELECT MAX(numViolations) FROM USERS;
 -- add group by query
 
 -- joins the message and user tables to show the username of the the message sent
