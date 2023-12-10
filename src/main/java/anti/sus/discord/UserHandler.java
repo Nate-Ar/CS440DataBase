36d4cb88a59d5e9ed0da019b8c847431300efecf
Mega <mega+signer@aleeas.com>
2023-12-10 16:20:11 -0600

    Add all users in server to the users table when the bot is added to a server.

--- src/main/java/anti/sus/discord/UserHandler.java
+++ src/main/java/anti/sus/discord/UserHandler.java
@@ -2,6 +2,7 @@ package anti.sus.discord;
 
 import anti.sus.database.DatabaseStorage;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -23,5 +24,20 @@ public final class UserHandler extends ListenerAdapter {
         final String userName = user.getName();
         final SqlQuery addUser = safeQuery("INSERT IGNORE INTO USERS VALUES (?, ?, DEFAULT);", userID, userName);
         this.databaseStorage.update(addUser, null);
+    }
+
+    @Override
+    public void onGuildJoin(@NotNull final GuildJoinEvent event) {
+        event.getGuild().getMembers().forEach(member -> {
+            if (member.getUser().isBot()) {
+                return;
+            }
+
+            final long userId = member.getIdLong();
+            final String userName = member.getUser().getName();
+            final SqlQuery addExistingUserQuery = safeQuery("INSERT IGNORE INTO USERS VALUES (?, ?, DEFAULT);", userId, userName);
+
+            this.databaseStorage.update(addExistingUserQuery, null);
+        });
     }
 }
